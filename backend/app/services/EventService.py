bd7f873394550dff5329ce60b0619f35fa684bbc
Mak7-kurochka <maksym.kurochka@gmail.com>
2025-10-08 21:09:34 +0200

    Correct columns names'

--- backend/app/services/EventService.py
+++ backend/app/services/EventService.py
@@ -2,18 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 from typing import List, Optional
-
 from ..schemas.event import EventQueryParams, EventOut
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.linear_model import PoissonRegressor
 from lightgbm import LGBMRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 import datetime
 
 
@@ -127,15 +123,15 @@ class EventService:
             return stat
     
     def predict_event_count(self,df=None,city=None):
-        df_long = self.df[['WEEK','ADMIN1']].copy()
+        df_long = self.df[['date','city']].copy()
         df_long.head()
         if city:
             cities = [city]
         else:
-            cities = df_long['ADMIN1'].unique()
+            cities = df_long['city'].unique()
         event_count = dict()
         for city in cities:
-            model = ARIMA(df_long[df_long['ADMIN1']==city]['WEEK'].value_counts().sort_index(), order=(2, 1, 2))  # you can tune (p,d,q)
+            model = ARIMA(df_long[df_long['city']==city]['date'].value_counts().sort_index(), order=(2, 1, 2))
             model_fit = model.fit()
             forecast = model_fit.forecast(steps=1)
             event_count[city] = np.round(forecast.iloc[0])
@@ -143,15 +139,15 @@ class EventService:
 
     def predict_coord(self,df=None,city=None):
 
-        df_coord = self.df[['WEEK', 'ADMIN1', 'CENTROID_LATITUDE', 'CENTROID_LONGITUDE']].copy()
+        df_coord = self.df[['date', 'city', 'lat', 'lon']].copy()
 
         encoder = OneHotEncoder(sparse_output=False)
-        X_admin = encoder.fit_transform(self.df[['ADMIN1']])
+        X_admin = encoder.fit_transform(self.df[['city']])
 
-        df_coord['week_num'] = df_coord['WEEK'].dt.isocalendar().week
+        df_coord['week_num'] = df_coord['date'].dt.isocalendar().week
 
         X = np.hstack([X_admin, df_coord[['week_num']].values])
-        y = self.df[['CENTROID_LATITUDE', 'CENTROID_LONGITUDE']]
+        y = self.df[['lat', 'lon']]
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -160,17 +156,17 @@ class EventService:
 
         y_pred = model.predict(X_test)
 
-        next_week = pd.to_datetime(df_coord['WEEK'].iloc[-1] + datetime.timedelta(days=7)) 
+        next_week = pd.to_datetime(df_coord['date'].iloc[-1] + datetime.timedelta(days=7)) 
 
         week_num = next_week.isocalendar()[1]
-        cities = df_coord['ADMIN1'].unique()
+        cities = df_coord['city'].unique()
 
         coord = dict()
 
         if city:
             cities = [city]
         else:
-            cities = df_coord['ADMIN1'].unique()
+            cities = df_coord['city'].unique()
 
         for city in cities:
             X_new = np.hstack([encoder.transform([[city]]), [[week_num]]])
@@ -187,89 +183,89 @@ class EventService:
             for i in range(int(count)):
                 rows.append([city])
 
-        df_pred = pd.DataFrame(rows, columns=['City'])
+        df_pred = pd.DataFrame(rows, columns=['city'])
         df_pred['WEEK_NUM'] = 39
 
-        df_pred['CENTROID_LATITUDE'] = df_pred['City'].apply(lambda x: df_coord[x][0])
-        df_pred['CENTROID_LONGITUDE'] = df_pred['City'].apply(lambda x: df_coord[x][1])
+        df_pred['lat'] = df_pred['city'].apply(lambda x: df_coord[x][0])
+        df_pred['lon'] = df_pred['city'].apply(lambda x: df_coord[x][1])
 
         lags = 3
         lag_values = {}
 
-        for city, group in self.df.groupby('ADMIN1'):
+        for city, group in self.df.groupby('city'):
 
-            group = group.sort_values('WEEK')
+            group = group.sort_values('date')
 
 
-            last_vals = group['EVENTS'].tail(lags).tolist()
+            last_vals = group['events'].tail(lags).tolist()
 
             lag_values[city] = last_vals
 
-        for col in ['EVENTS', 'FATALITIES']:
+        for col in ['events', 'fatalities']:
             for i in range(1, lags+1):
-                df_pred[f'{col}_lag{i}'] = df_pred['City'].apply(lambda city: lag_values[city][-i])
+                df_pred[f'{col}_lag{i}'] = df_pred['city'].apply(lambda city: lag_values[city][-i])
 
 
-        for col in ['EVENTS', 'FATALITIES']:
+        for col in ['events', 'fatalities']:
             df_pred[f'{col}_rolling_mean'] = (
-                self.df.sort_values(['ADMIN1', 'WEEK'])      
-                  .groupby('ADMIN1')[col]                
+                self.df.sort_values(['city', 'date'])      
+                  .groupby('city')[col]                
                   .transform(lambda x: x.shift(1)        
                                      .rolling(3, min_periods=1)
                                      .mean())
             )
 
-        rolling_cols = ['EVENTS_rolling_mean', 'FATALITIES_rolling_mean']
-        df_pred[rolling_cols] = df_pred.groupby('City')[rolling_cols].transform(lambda x: x.bfill().ffill())
+        rolling_cols = ['events_rolling_mean', 'fatalities_rolling_mean']
+        df_pred[rolling_cols] = df_pred.groupby('city')[rolling_cols].transform(lambda x: x.bfill().ffill())
 
     
         for col in rolling_cols:
             df_pred.iloc[-1, df_pred.columns.get_loc(col)] = df_pred.iloc[-2][col]
 
-        df_pred = pd.get_dummies(df_pred, columns=['City'], prefix='ADMIN1')
+        df_pred = pd.get_dummies(df_pred, columns=['city'], prefix='city')
 
         df_pred = df_pred[sorted(df_pred.columns)]
 
         return df_pred
     
     def predict_event_type(self, df=None,unencoded=False):
-        df_event = self.df[['WEEK', 'ADMIN1', 'EVENT_TYPE', 'EVENTS', 'FATALITIES', 'CENTROID_LATITUDE', 'CENTROID_LONGITUDE']].copy()
+        df_event = self.df[['date', 'city', 'event_type', 'events', 'fatalities', 'lat', 'lon']].copy()
     
         lags = 3 
-        for col in ['EVENTS','FATALITIES']:
+        for col in ['events', 'fatalities']:
             for lag in range(1, lags+1):
-                df_event[f'{col}_lag{lag}'] = df_event.groupby('ADMIN1')[col].shift(lag)
+                df_event[f'{col}_lag{lag}'] = df_event.groupby('city')[col].shift(lag)
 
 
-        lag_cols = [f'{col}_lag{lag}' for col in ['EVENTS','FATALITIES'] for lag in range(1, lags+1)]
+        lag_cols = [f'{col}_lag{lag}' for col in ['events', 'fatalities'] for lag in range(1, lags+1)]
 
         window = 3 
 
 
-        for col in ['EVENTS', 'FATALITIES']:
-            df_event[f'{col}_rolling_mean'] = df_event.groupby('ADMIN1')[col].transform(
+        for col in ['events', 'fatalities']:
+            df_event[f'{col}_rolling_mean'] = df_event.groupby('city')[col].transform(
                 lambda x: x.shift(1).rolling(window, min_periods=1).mean()
             )
 
-        df_event[lag_cols] = df_event.groupby('ADMIN1')[lag_cols].transform(lambda x: x.bfill())
+        df_event[lag_cols] = df_event.groupby('city')[lag_cols].transform(lambda x: x.bfill())
 
         cols_to_fill = [col for col in df_event.columns if 'lag' in col or 'rolling' in col]
 
 
-        df_event[cols_to_fill] = df_event.groupby('ADMIN1')[cols_to_fill].transform(lambda x: x.bfill().ffill())
+        df_event[cols_to_fill] = df_event.groupby('city')[cols_to_fill].transform(lambda x: x.bfill().ffill())
 
 
-        df_event = df_event.drop(['EVENTS','FATALITIES'],axis=1)
+        df_event = df_event.drop(['events', 'fatalities'],axis=1)
 
 
-        df_event = pd.get_dummies(df_event, columns=['ADMIN1'], prefix='ADMIN1')
-        df_event = pd.get_dummies(df_event, columns=['EVENT_TYPE'], prefix='EVENT_TYPE')
-        df_event['WEEK_NUM'] = df_event['WEEK'].dt.isocalendar().week
-        df_event = df_event.drop('WEEK',axis=1)
+        df_event = pd.get_dummies(df_event, columns=['city'], prefix='city')
+        df_event = pd.get_dummies(df_event, columns=['event_type'], prefix='event_type')
+        df_event['WEEK_NUM'] = df_event['date'].dt.isocalendar().week
+        df_event = df_event.drop('date',axis=1)
 
-        admin_cols = [col for col in df_event.columns if col.startswith('ADMIN1_')]
-        event_type_cols = [col for col in df_event.columns if col.startswith('EVENT_TYPE_')]
-        feature_cols = ['WEEK_NUM', 'CENTROID_LATITUDE', 'CENTROID_LONGITUDE','EVENTS_rolling_mean', 'FATALITIES_rolling_mean'] + [f'{col}_lag{lag}' for col in ['EVENTS','FATALITIES'] for lag in range(1, lags+1)] + admin_cols
+        admin_cols = [col for col in df_event.columns if col.startswith('city')]
+        event_type_cols = [col for col in df_event.columns if col.startswith('event_type')]
+        feature_cols = ['WEEK_NUM', 'lat', 'lon','events_rolling_mean', 'fatalities_rolling_mean'] + [f'{col}_lag{lag}' for col in ['events', 'fatalities'] for lag in range(1, lags+1)] + admin_cols
 
 
 
@@ -308,34 +304,34 @@ class EventService:
         return pd.concat([x,y], axis=1)
     
     def predict_fatalities(self, X_new, df=None):
-        df_fatal = self.df[['ADMIN1','WEEK', 'CENTROID_LATITUDE', 'CENTROID_LONGITUDE', 'EVENTS', 'FATALITIES','EVENT_TYPE']].copy()
+        df_fatal = self.df[['city','date', 'lat', 'lon', 'events', 'fatalities','event_type']].copy()
         lags = 3 
-        for col in ['EVENTS','FATALITIES']:
+        for col in ['events', 'fatalities']:
             for lag in range(1, lags+1):
-                df_fatal[f'{col}_lag{lag}'] = df_fatal.groupby('ADMIN1')[col].shift(lag)
-        lag_cols = [f'{col}_lag{lag}' for col in ['EVENTS','FATALITIES'] for lag in range(1, lags+1)]
+                df_fatal[f'{col}_lag{lag}'] = df_fatal.groupby('city')[col].shift(lag)
+        lag_cols = [f'{col}_lag{lag}' for col in ['events', 'fatalities'] for lag in range(1, lags+1)]
         window = 3 
-        for col in ['EVENTS', 'FATALITIES']:
-            df_fatal[f'{col}_rolling_mean'] = df_fatal.groupby('ADMIN1')[col].transform(
+        for col in ['events', 'fatalities']:
+            df_fatal[f'{col}_rolling_mean'] = df_fatal.groupby('city')[col].transform(
                 lambda x: x.shift(1).rolling(window, min_periods=1).mean()
             )
-        df_fatal[lag_cols] = df_fatal.groupby('ADMIN1')[lag_cols].transform(lambda x: x.bfill())
+        df_fatal[lag_cols] = df_fatal.groupby('city')[lag_cols].transform(lambda x: x.bfill())
         cols_to_fill = [col for col in df_fatal.columns if 'lag' in col or 'rolling' in col]
-        df_fatal[cols_to_fill] = df_fatal.groupby('ADMIN1')[cols_to_fill].transform(lambda x: x.bfill().ffill())
+        df_fatal[cols_to_fill] = df_fatal.groupby('city')[cols_to_fill].transform(lambda x: x.bfill().ffill())
 
-        df_fatal = pd.get_dummies(df_fatal, columns=['ADMIN1'], prefix='ADMIN1')
-        df_fatal = pd.get_dummies(df_fatal, columns=['EVENT_TYPE'], prefix='EVENT_TYPE')
+        df_fatal = pd.get_dummies(df_fatal, columns=['city'], prefix='city')
+        df_fatal = pd.get_dummies(df_fatal, columns=['event_type'], prefix='event_type')
 
-        df_fatal['WEEK_NUM'] = df_fatal['WEEK'].dt.isocalendar().week
-        df_fatal = df_fatal.drop('WEEK',axis=1)
+        df_fatal['WEEK_NUM'] = df_fatal['date'].dt.isocalendar().week
+        df_fatal = df_fatal.drop('date',axis=1)
 
-        admin_cols = [col for col in df_fatal.columns if col.startswith('ADMIN1_')]
-        event_cols = [col for col in df_fatal.columns if col.startswith('EVENT_TYPE_')]
-        feature_cols = ['WEEK_NUM', 'CENTROID_LATITUDE', 'CENTROID_LONGITUDE','EVENTS_rolling_mean', 'FATALITIES_rolling_mean'] + [f'{col}_lag{lag}' for col in ['EVENTS','FATALITIES'] for lag in range(1, lags+1)] + admin_cols + event_cols
+        admin_cols = [col for col in df_fatal.columns if col.startswith('city_')]
+        event_cols = [col for col in df_fatal.columns if col.startswith('event_type_')]
+        feature_cols = ['WEEK_NUM', 'lat', 'lon','events_rolling_mean', 'fatalities_rolling_mean'] + [f'{col}_lag{lag}' for col in ['events', 'fatalities'] for lag in range(1, lags+1)] + admin_cols + event_cols
 
         X = df_fatal[sorted(feature_cols)]
-        y_events = df_fatal['EVENTS']
-        y_fatalities = df_fatal['FATALITIES']
+        y_events = df_fatal['events']
+        y_fatalities = df_fatal['fatalities']
 
         X_train, X_test, y_train_events, y_test_events = train_test_split(X, y_events, test_size=0.2, shuffle=False)
         _, _, y_train_fatal, y_test_fatal = train_test_split(X, y_fatalities, test_size=0.2, shuffle=False)
@@ -366,7 +362,6 @@ class EventService:
 
         )
 
-        # FATALITIES model
         fatal_model = LGBMRegressor(
             objective='poisson',
             n_estimators=1000,
@@ -387,8 +382,8 @@ class EventService:
         y_pred_fatal = fatal_model.predict(X_new)
 
         # Clip predictions to avoid negative counts
-        y_pred_events = pd.DataFrame(np.round(np.clip(y_pred_events, 0, None)),columns=['Events'])
-        y_pred_fatal = pd.DataFrame(np.round(np.clip(y_pred_fatal, 0, None)), columns=['Fatalities'])
+        y_pred_events = pd.DataFrame(np.round(np.clip(y_pred_events, 0, None)),columns=['events'])
+        y_pred_fatal = pd.DataFrame(np.round(np.clip(y_pred_fatal, 0, None)), columns=['fatalities'])
 
         prediction = pd.concat([y_pred_events, y_pred_fatal],axis=1)
         return prediction
@@ -403,13 +398,13 @@ class EventService:
         event_cols = ['Battles', 'Explosions/Remote violence', 'Protests', 
                   'Riots', 'Strategic developments', 'Violence against civilians']
 
-        pred['EVENT_TYPE'] = pred[event_cols].idxmax(axis=1)
+        pred['event_type'] = pred[event_cols].idxmax(axis=1)
 
-        admin_cols = [col for col in pred.columns if col.startswith('ADMIN1_')]
+        admin_cols = [col for col in pred.columns if col.startswith('city_')]
 
-        pred['ADMIN1'] = pred[admin_cols].idxmax(axis=1).str.replace('ADMIN1_', '')
+        pred['city'] = pred[admin_cols].idxmax(axis=1).str.replace('city_', '')
 
-        cols_to_drop = event_cols + admin_cols +['EVENTS_lag1', 'EVENTS_lag2', 'EVENTS_lag3', 'EVENTS_rolling_mean', 'FATALITIES_lag1', 'FATALITIES_lag2', 'FATALITIES_lag3', 'FATALITIES_rolling_mean']
+        cols_to_drop = event_cols + admin_cols +['events_lag1', 'events_lag2', 'events_lag3', 'events_rolling_mean', 'fatalities_lag1', 'fatalities_lag2', 'fatalities_lag3', 'fatalities_rolling_mean']
 
         year = 2025
         week = 39
@@ -417,7 +412,7 @@ class EventService:
         pred = pred.drop(columns=cols_to_drop)
         pred['WEEK_NUM'] = week_end
 
-        new_order=['WEEK_NUM', 'ADMIN1','EVENT_TYPE','Events','Fatalities','CENTROID_LATITUDE','CENTROID_LONGITUDE']
+        new_order=['WEEK_NUM', 'city','event_type','events','fatalities','lat','lon']
 
         pred = pred[new_order]
 
